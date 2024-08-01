00a5ec2aa87407315ce4e3459235183830550306
leejimin3 <ttette1559@naver.com>
2024-08-02 00:45:04 +0900

    [ADD] ETC Section

--- payload/education.ts
+++ payload/education.ts
@@ -1,22 +1,22 @@
 import { IEducation } from '../component/education/IEducation';
 
 const education: IEducation.Payload = {
-    disable: false,
+  disable: false,
 
-    list: [
-        {
-            title: '안양대학교',
-            subTitle: '컴퓨터공학과 졸업',
-            startedAt: '2017-03',
-            endedAt: '2023-06',
-        },
-        {
-            title: '부평고등학교',
-            subTitle: '자연계 졸업',
-            startedAt: '2014-03',
-            endedAt: '2016-12',
-        },
-    ],
+  list: [
+    {
+      title: '안양대학교',
+      subTitle: '컴퓨터공학과 졸업',
+      startedAt: '2017-03',
+      endedAt: '2023-06',
+    },
+    {
+      title: '부평고등학교',
+      subTitle: '자연계 졸업',
+      startedAt: '2014-03',
+      endedAt: '2016-12',
+    },
+  ],
 };
 
 export default education;
--- payload/presentation.ts
+++ payload/presentation.ts
@@ -1,24 +1,24 @@
 import { IPresentation } from '../component/presentation/IPresentation';
 
 const presentation: IPresentation.Payload = {
-    disable: false,
+  disable: false,
 
-    list: [
+  list: [
+    {
+      title: '내가 "존중과 배움"을 통해 갈등을 해결하는 방법',
+      subTitle: '네이버 SEF2023 컨퍼런스 [토크 버스킹]',
+      at: '2023-10',
+      descriptions: [
         {
-            title: '내가 "존중과 배움"을 통해 갈등을 해결하는 방법',
-            subTitle: '네이버 SEF2023 컨퍼런스 [토크 버스킹]',
-            at: '2023-10',
-            descriptions: [
-                {
-                    content: '2차 면접을 통해 선별된 버스킹 내용을 발표',
-                },
-                {
-                    content: '영상 링크 [25초 ~ 32초]: ',
-                    postHref: 'https://youtu.be/EG4EocHJbdk?feature=shared&t=25',
-                },
-            ],
+          content: '2차 면접을 통해 선별된 버스킹 내용을 발표',
         },
-    ],
+        {
+          content: '영상 링크 [25초 ~ 32초]: ',
+          postHref: 'https://youtu.be/EG4EocHJbdk?feature=shared&t=25',
+        },
+      ],
+    },
+  ],
 };
 
 export default presentation;
