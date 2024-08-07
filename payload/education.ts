98825483b91ef97380a8ed785948886f0be35ff8
leejimin3 <ttette1559@naver.com>
2024-08-08 02:21:42 +0900

    [FIX] Personalstatement Page

--- payload/education.ts
+++ payload/education.ts
@@ -6,7 +6,7 @@ const education: IEducation.Payload = {
   list: [
     {
       title: '안양대학교',
-      subTitle: '컴퓨터공학과 졸업',
+      subTitle: '컴퓨터공학과 졸업 (학점 : [3.7 / 4.5])',
       startedAt: '2017-03',
       endedAt: '2023-06',
     },
